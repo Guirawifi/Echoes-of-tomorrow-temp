89dfe47318a1770f2f8796662f0685a981227ba8
Guirawifi <jeanmel187@gmail.com>
2024-08-06 13:06:57 +0200

    Discord rich presence + coyote time

--- Player.gd
+++ Player.gd
@@ -7,6 +7,14 @@ const JUMP_VELOCITY = -500.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 980
 @onready var sprite_2d = $Sprite2D
+var was_on_floor = false
+var coyote_time = 9999999999.99
+
+func _ready():
+	DiscordRPC.details = "In the game"
+	DiscordRPC.state = "So many bugs ;-;"
+	
+	DiscordRPC.refresh()
 
 func _physics_process(delta):
 	if (velocity.x > 1 || velocity.x < -1):
@@ -16,27 +24,35 @@ func _physics_process(delta):
 		
 	# Add the gravity.
 	if not is_on_floor():
+		if was_on_floor:
+			was_on_floor = false
+			coyote_time = Time.get_unix_time_from_system() + 0.1
+			
 		if velocity.y < 0:
 			gravity = 980
 		else:
 			gravity = 1300
 		velocity.y += gravity * delta
 		sprite_2d.animation = "Jumping"
+	else:
+		was_on_floor = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	if (Input.is_action_just_released("ui_accept") and velocity.y < 0):
+	elif Input.is_action_just_pressed("jump") and Time.get_unix_time_from_system() <= coyote_time:
+		velocity.y = JUMP_VELOCITY
+	if (Input.is_action_just_released("jump") and velocity.y < 0):
 		velocity.y = 0
 	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 30)
 
 	move_and_slide()
 	

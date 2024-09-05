5e9a136d545d46f698cd3260d330bebfb68d78e8
Guirawifi <129207186+Guirawifi@users.noreply.github.com>
2024-09-05 19:49:59 +0200

    grass sync (start)

---
+++ Grass.gd
@@ -0,0 +1,27 @@
+extends TileMap
+
+var wait_time = Time.get_unix_time_from_system() + randi_range(1, 3)*1.2
+var animating = false
+var time = Time.get_unix_time_from_system()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_layer_enabled(0, false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	time = Time.get_unix_time_from_system()
+	if time >= wait_time and not animating:
+		wait_time = time + 1.2
+		set_layer_enabled(1, false)
+		set_layer_enabled(0, true)
+		animating = true
+		print("start")
+	if animating and time >= wait_time:
+		animating = false
+		set_layer_enabled(1, true)
+		set_layer_enabled(0, false)
+		wait_time = time + randi_range(1, 3)*1.2
+		print((wait_time - time)/1.2)
+		print("end")
